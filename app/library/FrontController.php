328ce521f23106ec7eba37b91071bba1052fe346
Taras <landgradus@gmail.com>
2017-11-15 10:43:37 +0300

    Add check for the existance of class and method in FrontController

--- app/library/FrontController.php
+++ app/library/FrontController.php
@@ -13,12 +13,37 @@ class FrontController
     public function run()
     {
         $route = $_GET['route'] ?? 'home/index';
-        $parts = explode('/', $route);
+        $segments = explode('/', $route);
 
-        $controller_class = 'App\Controllers\\' . ucfirst($parts[0]) . 'Controller';
-        $controller_action = $parts[1];
+        $controller_class = $this->getControllerClassOrFail($segments[0]);
+        $controller_action = $this->getControllerActionOrFail($controller_class, $segments[1]);
 
         $controller = new $controller_class();
         echo $controller->$controller_action();
     }
+
+    private function getControllerClassOrFail($name)
+    {
+        $controller_class = 'App\Controllers\\' . ucfirst($name) . 'Controller';
+
+        if (class_exists($controller_class)) {
+            return $controller_class;
+        } else {
+            header("HTTP/1.0 404 Not Found");
+            echo 'Page not found!';
+            die;
+        }
+    }
+
+    private function getControllerActionOrFail($controller_class, $name)
+    {
+        if(method_exists($controller_class, $name)){
+            return $name;
+        }else{
+            header("HTTP/1.0 404 Not Found");
+            echo 'Page not found!';
+            die;
+        }
+    }
+
 }
