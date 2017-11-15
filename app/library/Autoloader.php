10d3e6979fb3101b8b79af31433779580cd0c182
Taras <landgradus@gmail.com>
2017-11-15 10:12:10 +0300

    Add a method to get the path to class file"

--- app/library/Autoloader.php
+++ app/library/Autoloader.php
@@ -14,17 +14,22 @@ class Autoloader
     {
         spl_autoload_register(function ($class_name) {
 
-            $parts = explode('\\', $class_name);
-
-            for ($i = 0; $i < count($parts) - 1; $i++) {
-                $parts[$i] = strtolower($parts[$i]);
-            }
-
-            $file_path = ROOT . implode(DIRECTORY_SEPARATOR, $parts) . '.php';
+            $file_path = $this->getFilePath($class_name);
 
             if (file_exists($file_path)) {
                 require_once $file_path;
             }
         });
+    }
+
+    protected function getFilePath($class_name)
+    {
+        $parts = explode('\\', $class_name);
+
+        for ($i = 0; $i < count($parts) - 1; $i++) {
+            $parts[$i] = strtolower($parts[$i]);
+        }
+
+        return ROOT . implode(DIRECTORY_SEPARATOR, $parts) . '.php';
     }
 }
