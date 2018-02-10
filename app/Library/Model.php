ed4b7b9cf87cfd693eed093d2a2c27e3871335bb
Taras <landgradus@gmail.com>
2018-02-10 16:56:07 +0300

    Add a class to connect to the database

--- app/Library/Model.php
+++ app/Library/Model.php
@@ -1,26 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: taras
- * Date: 19.11.17
- * Time: 9:49
- */
 
 namespace App\Library;
 
-use App\Config;
 use PDO;
 
 class Model
 {
-    /**
-     * Options of the PDO connection
-     */
-    const DB_OPTIONS = [
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
-    ];
-
     /**
      * @var PDO
      */
@@ -31,23 +16,6 @@ class Model
      */
     public function __construct()
     {
-        $this->databaseConnection();
-    }
-
-    /**
-     * Open the database connection with credentials from App\Config
-     */
-    private function databaseConnection()
-    {
-        $dsn = "mysql:host=" . Config::DB_HOSTNAME . ";" .
-            "port=" . Config::DB_PORT . ";" .
-            "dbname=" . Config::DB_DATABASE;
-
-        $this->db = new PDO(
-            $dsn,
-            Config::DB_USERNAME,
-            Config::DB_PASSWORD,
-            self::DB_OPTIONS
-        );
+        $this->db = Db::getInstance()->getConnection();
     }
 }
